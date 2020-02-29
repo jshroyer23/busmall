958637016613fd5841b287d501ecad47613d7681
Justin Shroyer <hjshroyer@hotmail.com>
2020-02-29 13:47:24 -0600

    Added JS to store results to localStorage and pull from localStorage on the next instance

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var totalClicks = 0;
 var rounds = 25;
 
 // Constructor
-function Product(name, url){
+function Product(name, url) {
   this.name = name;
   this.productUrl = url;
   this.timesClicked = 0;
@@ -18,35 +18,47 @@ function Product(name, url){
   allProducts.push(this);
 }
 
-function getProductArray(productProperty){
+function getProductArray(productProperty) {
   var answer = [];
-  for(var i = 0; i < allProducts.length; i++){
+  for (var i = 0; i < allProducts.length; i++) {
     answer[i] = allProducts[i][productProperty];
   }
   return answer;
 }
 
-// Create Products
-new Product('Bag', 'img/bag.jpg');
-new Product('Banana Slicer', 'img/banana.jpg');
-new Product('Bathroom Accessory', 'img/bathroom.jpg');
-new Product('Toeless Boots', 'img/boots.jpg');
-new Product('Breakfast Maker', 'img/breakfast.jpg');
-new Product('Meatball Bubble Gum', 'img/bubblegum.jpg');
-new Product('Funky Chair', 'img/chair.jpg');
-new Product('Cthulhu Action Figure', 'img/cthulhu.jpg');
-new Product('Dog Duck Lips', 'img/dog-duck.jpg');
-new Product('Dragon Meat', 'img/dragon.jpg');
-new Product('Silverware Pens', 'img/pen.jpg');
-new Product('Pet Sweeper', 'img/pet-sweep.jpg');
-new Product('Pizza Scissors', 'img/scissors.jpg');
-new Product('Shark Sleeping Bag', 'img/shark.jpg');
-new Product('Baby Sweeper', 'img/sweep.png');
-new Product('Tauntaun Sleeping Bag', 'img/tauntaun.jpg');
-new Product('Unicorn Meat', 'img/unicorn.jpg');
-new Product('Tentacle Thumbdrive', 'img/usb.gif');
-new Product('Watering Can', 'img/water-can.jpg');
-new Product('Wine Glass', 'img/wine-glass.jpg');
+var savedProducts = localStorage.getItem('savedProduct');
+// Check to see if anything in localStorage
+if (savedProducts) {
+  var productStrObject = JSON.parse(savedProducts);
+
+  // Turn the string obj into a Product obj
+  for (var i = 0; i < productStrObject.length; i++) {
+    new Product(productStrObject[i].name, productStrObject[i].productUrl, productStrObject[i].timesClicked, productStrObject[i].timesSeen);
+  }
+}
+else {
+  // Create Products
+  new Product('Bag', 'img/bag.jpg');
+  new Product('Banana Slicer', 'img/banana.jpg');
+  new Product('Bathroom Accessory', 'img/bathroom.jpg');
+  new Product('Toeless Boots', 'img/boots.jpg');
+  new Product('Breakfast Maker', 'img/breakfast.jpg');
+  new Product('Meatball Bubble Gum', 'img/bubblegum.jpg');
+  new Product('Funky Chair', 'img/chair.jpg');
+  new Product('Cthulhu Action Figure', 'img/cthulhu.jpg');
+  new Product('Dog Duck Lips', 'img/dog-duck.jpg');
+  new Product('Dragon Meat', 'img/dragon.jpg');
+  new Product('Silverware Pens', 'img/pen.jpg');
+  new Product('Pet Sweeper', 'img/pet-sweep.jpg');
+  new Product('Pizza Scissors', 'img/scissors.jpg');
+  new Product('Shark Sleeping Bag', 'img/shark.jpg');
+  new Product('Baby Sweeper', 'img/sweep.png');
+  new Product('Tauntaun Sleeping Bag', 'img/tauntaun.jpg');
+  new Product('Unicorn Meat', 'img/unicorn.jpg');
+  new Product('Tentacle Thumbdrive', 'img/usb.gif');
+  new Product('Watering Can', 'img/water-can.jpg');
+  new Product('Wine Glass', 'img/wine-glass.jpg');
+}
 
 // Account for first three Products being seen
 allProducts[productIndex1].timesSeen++;
@@ -55,7 +67,7 @@ allProducts[productIndex3].timesSeen++;
 
 function productClicked(event) {
   totalClicks++;
-  if(event.srcElement.id === '1') {
+  if (event.srcElement.id === '1') {
     allProducts[productIndex1].timesClicked++;
   }
   else if (event.srcElement.id === '2') {
@@ -67,17 +79,17 @@ function productClicked(event) {
   // Choose random product to display
   var nextProductIndex1 = Math.floor(Math.random() * allProducts.length);
   // To ensure that we don't get an image duplicate on the page, and that we dont see it again on the next rotation. Compare the new first index with the existing first, second, and third product indexes.
-  while((nextProductIndex1 === productIndex1) || (nextProductIndex1 === productIndex2) || (nextProductIndex1 === productIndex3)) {
+  while ((nextProductIndex1 === productIndex1) || (nextProductIndex1 === productIndex2) || (nextProductIndex1 === productIndex3)) {
     nextProductIndex1 = Math.floor(Math.random() * allProducts.length);
   }
   var nextProductIndex2 = Math.floor(Math.random() * allProducts.length);
   // Compares the new second index with existing first, second, and third product indexes, along with the next first index.
-  while((nextProductIndex2 === productIndex1) || (nextProductIndex2 === productIndex2) || (nextProductIndex2 === productIndex3) || (nextProductIndex2 === nextProductIndex1)) {
+  while ((nextProductIndex2 === productIndex1) || (nextProductIndex2 === productIndex2) || (nextProductIndex2 === productIndex3) || (nextProductIndex2 === nextProductIndex1)) {
     nextProductIndex2 = Math.floor(Math.random() * allProducts.length);
   }
   var nextProductIndex3 = Math.floor(Math.random() * allProducts.length);
   // Compare the the new third index with existing first, second, and third product indexes, along with the next first and second product indexes.
-  while((nextProductIndex3 === productIndex1) || (nextProductIndex3 === productIndex2) || (nextProductIndex3 === productIndex3) || (nextProductIndex3 === nextProductIndex1) || (nextProductIndex3 === nextProductIndex2)) {
+  while ((nextProductIndex3 === productIndex1) || (nextProductIndex3 === productIndex2) || (nextProductIndex3 === productIndex3) || (nextProductIndex3 === nextProductIndex1) || (nextProductIndex3 === nextProductIndex2)) {
     nextProductIndex3 = Math.floor(Math.random() * allProducts.length);
   }
 
@@ -93,24 +105,27 @@ function productClicked(event) {
   productElements[1].src = allProducts[productIndex2].productUrl;
   productElements[2].src = allProducts[productIndex3].productUrl;
 
-  if(totalClicks >= rounds) {
+  if (totalClicks >= rounds) {
     // We reached the maximum number of clicks designated by rounds
+
+    localStorage.setItem('savedProduct', JSON.stringify(allProducts));
+
     var resultsEl = document.getElementsByTagName('aside')[0];
-    if(resultsEl.firstElementChild){
+    if (resultsEl.firstElementChild) {
       resultsEl.firstElementChild.remove();
     }
     var title = document.createElement('h2');
     title.textContent = 'Results';
     resultsEl.appendChild(title);
     var createUL = document.createElement('ul');
-    for (var i=0; i < allProducts.length; i++){
+    for (var i = 0; i < allProducts.length; i++) {
       var createLI = document.createElement('li');
       createLI.textContent = allProducts[i].name + ' had ' + allProducts[i].timesClicked + ' votes and was shown ' + allProducts[i].timesSeen + ' times.';
       createUL.appendChild(createLI);
     }
     resultsEl.appendChild(createUL);
   }
-  if(totalClicks === rounds) {
+  if (totalClicks === rounds) {
     for (var j = 0; j < productElements.length; j++) {
       productElements[j].removeEventListener('click', productClicked);
     }
@@ -119,10 +134,10 @@ function productClicked(event) {
 }
 
 // Creates array of colors
-function colorSet(colorType){
+function colorSet(colorType) {
   var colorArray = [];
   console.log(allProducts.length);
-  for (var i = 0; i < allProducts.length; i++){
+  for (var i = 0; i < allProducts.length; i++) {
     colorArray.push(randomColor(colorType));
   }
   console.log(colorArray);
@@ -130,14 +145,14 @@ function colorSet(colorType){
 }
 
 // Random color generator based on if backgound or border color
-function randomColor(colorType){
-  var r = Math.floor(Math.random()*256);
-  var g = Math.floor(Math.random()*256);
-  var b = Math.floor(Math.random()*256);
-  if (colorType === 'background'){
+function randomColor(colorType) {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  if (colorType === 'background') {
     var color = 'rgb(' + r + ',' + g + ',' + b + ', 0.2)';
   }
-  else if(colorType === 'border'){
+  else if (colorType === 'border') {
     color = 'rgb(' + r + ',' + g + ',' + b + ', 1)';
   }
   return color;
